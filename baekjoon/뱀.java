9b8804db7f1f581330574904512f6b7edb4c306f
김도예 <kmdy125@gmail.com>
2024-01-18 22:25:35 +0900

    Update 뱀.java

--- baekjoon/뱀.java
+++ baekjoon/뱀.java
@@ -11,16 +11,6 @@ class Solution
 		int[][] apple; // 사과 위치
 		int[] turnTime; // 회전 시간
 		char[] turnDir; // 회전 방향
-		boolean isEnd = false;
-		
-		int[] dirX = {-1, 1, 0, 0}; // UDLR 
-		int[] dirY = {0, 0, -1, 1};
-		
-		char[] dir = {'U', 'D', 'L', 'R'};
-		char[] dirL = {'L', 'R', 'D', 'U'}; //{2, 3, 1, 0}; // LRDU
-		char[] dirR = {'R', 'L', 'U', 'D'}; //{3, 2, 0, 1}; //RLUD
-		
-		Queue<int[]> snake = new LinkedList<>();
 		
 		// input
 		n = Integer.parseInt(bf.readLine());
@@ -40,39 +30,40 @@ class Solution
 			turnTime[i] = Integer.parseInt(a[0]);
 			turnDir[i] = a[1].charAt(0);
 		}
-		//System.out.println(Arrays.toString(dir));
+		
+		// 구현 
+		Queue<int[]> snake = new LinkedList<>();
+		
+		boolean isEnd = false;
+		
+		int[] dirX = {-1, 0, 1, 0};
+		int[] dirY = {0, 1, 0, -1}; // 상우하좌  
+
 		int ti = 0; //회전 인덱스 
 		int nt = 1; // 지금 시간 
-		char nd = 'R'; // 지금 방향 
+		int nd = 1; // 지금 방향 
 		
 		int cx = 0, cy = 0;
 		snake.offer(new int[] {cx, cy});
-		
-		int index = 0;
-		//System.out.println(board[0][1]);
+
 		while(!isEnd) {
-			//System.out.println("c: " + cx + " " + cy + " nt: " + nt);
 			boolean eatApple = false;
-			for(int i = 0; i < 4; i++) {
-				if(dir[i] == nd) index = i;
-			}
-			//System.out.println(index);
-			int nx = cx + dirX[index];
-			int ny = cy + dirY[index];
+			
+			int nx = cx + dirX[nd];
+			int ny = cy + dirY[nd];
 			if(nx == n || ny == n || nx < 0 || ny < 0 || board[nx][ny] == 1) {
-				//System.out.println(nx + " " + ny + " " + board[nx][ny]);
 				isEnd = true;
 				break;
 			}
-			//몸 길이 늘려서 다음 칸으로 이동 처리, board가 1이면 뱀~ 
+			//몸 길이 늘려서 다음 칸으로 이동 처리, board가 1이면 뱀 
 			else{
 				snake.offer(new int[] {nx, ny});
 				board[nx][ny] = 1;
 				cx = nx;
 				cy = ny;
 			}
-			//System.out.println("board: " + cx + " " + cy +  " " +board[nx][ny]);
-			//사과 있을 때, 없을 때 구분
+
+			//사과 있을 때, 없을 때 
 			for(int i = 0; i < k; i++) {
 				//사과 있을 때! 
 				if(nx == apple[i][0] && ny == apple[i][1]) {
@@ -91,10 +82,13 @@ class Solution
 			if(ti < l && nt == turnTime[ti]) {
 				//System.out.println("회전합미다 ");
 				if(turnDir[ti] == 'L') {
-					nd = dirL[index];
+					nd--;
+					if(nd == -1) nd = 3;
 				}
 				if(turnDir[ti] == 'D') {
-					nd = dirR[index];
+					nd++;
+					if(nd == 4) nd = 0;
+					
 				}
 				ti++;
 			}
