c19baa315e992b08293cad61112383bb8ecd0177
김도예 <kmdy125@gmail.com>
2024-01-26 09:52:44 +0900

    Update 10282.java

--- baekjoon/10282.java
+++ baekjoon/10282.java
@@ -4,11 +4,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 
+
 public class Main {
-	static int n, d, c, cnt, max;
+	static int n, d, c, max;
 	static boolean[] isVisited;
+	static HashSet<Integer> cnt;
 	static ArrayList<Node>[] list;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,34 +23,37 @@ public class Main {
 			n = Integer.parseInt(str[0]);
 			d = Integer.parseInt(str[1]);
 			c = Integer.parseInt(str[2]) - 1;
-			cnt = 1;
+			//System.out.println(n + " " + d + " " + c);
 			isVisited = new boolean[n];
 			list = new ArrayList[n];
+			cnt = new HashSet<>();
 			for(int i = 0; i < n; i++){
 	            list[i] = new ArrayList<>();
 	        }
 	
 			for(int i = 0; i < d; i++) {
 				String[] s = br.readLine().split(" ");
+				//System.out.println(d + " " + Arrays.toString(s));
 				int a = Integer.parseInt(s[0]) - 1;
 				int b = Integer.parseInt(s[1]) - 1;
 				int t = Integer.parseInt(s[2]);
 				
 				list[b].add(new Node(a, t));
 			}
-			System.out.println(cnt + " " + dijk());
+			dijk();
 		}
 	}
-	static int dijk() {
+	static void dijk() {
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		int[] times = new int[n];
 		Arrays.fill(times, Integer.MAX_VALUE);
 		pq.add(new Node(c, 0));
-		isVisited[c] = true;
+		cnt.add(c);
 		times[c] = 0;
 
 		while(!pq.isEmpty()) {
 			Node pos = pq.poll();
+			//System.out.println(pos.a + " " + pos.time);
 			int na = pos.a;
 			if(isVisited[na]) continue;
 			isVisited[na] = true;
@@ -55,15 +61,19 @@ public class Main {
 				if(times[node.a] > times[na] + node.time) {
 					times[node.a] = times[na] + node.time;
 					pq.add(new Node(node.a, times[node.a]));
-					System.out.println(times[node.a]);
+					cnt.add(node.a);
+					//System.out.println(cnt);
+					//System.out.println(times[node.a]);
 				}
 			}
 		}
 		int answer = 0;
+		//System.out.println("time");
 		for(int i = 0; i < n; i++) {
+			if(times[i] == Integer.MAX_VALUE) continue;
 			answer = answer > times[i] ? answer : times[i];
 		}
-		return answer;
+		System.out.println(cnt.size() + " " + answer);
 	}
 }
 class Node implements Comparable<Node>{
