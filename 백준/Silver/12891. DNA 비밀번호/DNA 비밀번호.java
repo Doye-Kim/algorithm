553b0f108e98362fbfcd0eaa43be6d15e73a12ec
김도예 <kmdy125@gmail.com>
2024-02-01 17:29:43 +0900

    [Silver II] Title: DNA 비밀번호, Time: 276 ms, Memory: 21992 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -6,7 +6,9 @@ public class Main {
 
 	static int S, P, ans;
 	static char[] dna;
-	static int minA, minC, minG, minT, cntA, cntC, cntG, cntT;
+	static int minA, minC, minG, minT;
+	// A-T 빈도수 배열 (4개 제외 나머지는 걍 dummy)
+	static int[] cnt = new int[20];
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -26,37 +28,21 @@ public class Main {
 	// 문자열을 쭉 처리하면서 유효한지 검증
 	static void solve() {
 		// 맨 앞 P개 처리 <= P개 중 ACGT에 해당하는 문자이면 각각 해당하는 cntA, ... , cntT 를 증가
-		for(int i = 0; i < P; i++) {
-			switch(dna[i]) {
-				case 'A' : cntA++; break;
-				case 'C' : cntC++; break;
-				case 'G' : cntG++; break;
-				case 'T' : cntT++; break;
-			}
-		}
+		for(int i = 0; i < P; i++) cnt[dna[i] - 'A']++;
 		check();
 		// P 이후 한 개씩 처리
 		// 이전 P 개 중 맨 앞 문자 제거 -> 새 문자가 맨 뒤에 추가
 		for(int i = P; i < S; i++) {
 			// 맨 앞 제거
-			switch(dna[i - P]) {
-				case 'A' : cntA--; break;
-				case 'C' : cntC--; break;
-				case 'G' : cntG--; break;
-				case 'T' : cntT--; break;
-			}
+			cnt[dna[i - P] - 'A']--;
 			// 맨 뒤 추가
-			switch(dna[i]) {
-			case 'A' : cntA++; break;
-			case 'C' : cntC++; break;
-			case 'G' : cntG++; break;
-			case 'T' : cntT++; break;
-			}
+			cnt[dna[i] - 'A']++;
+
 			check();
 		}
 	}
 	// 현재 각 문자의 cnt 가 최소 조건을 만족하는지 확인 후 ans 증가
 	static void check() {
-		if(cntA >= minA && cntC >= minC && cntG >= minG && cntT >= minT) ans++;
+		if(cnt[0] >= minA && cnt[2] >= minC && cnt[6] >= minG && cnt[19] >= minT) ans++;
 	}
 }
