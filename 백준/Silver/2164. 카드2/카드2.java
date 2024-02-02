06321401282f7045204f490958dfb1a9f598fe58
김도예 <kmdy125@gmail.com>
2024-02-02 15:51:21 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 28968 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,12 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		Deque<Integer> queue = new ArrayDeque<>();
 		for(int i = 1; i <= n; i++) {
 			queue.offer(i);
