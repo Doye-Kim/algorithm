0194e97cffa54b79d426aeccecd3f11959b842b5
김도예 <kmdy125@gmail.com>
2024-02-06 10:41:50 +0900

    [Bronze II] Title: 소수 찾기, Time: 120 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int cnt = 0;
+		for(int i = 0; i < n; i++) {
+			if(isPrime(Integer.parseInt(st.nextToken()))) cnt++;
+		}
+		System.out.println(cnt);
+
+	}
+	static boolean isPrime(int num) {
+		if(num == 1) return false;
+		for(int i = 2; i <= Math.sqrt(num); i++) {
+			if(num % i == 0) return false;
+		}
+		return true;
+	}
+
+}
