ab093a03edd81349f57489ff08397c1330344d01
김도예 <kmdy125@gmail.com>
2024-02-06 12:03:59 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 123 ms, Memory: 19,440 KB -BaekjoonHub

---
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		for(int t = 1; t <= 10; t++) {
+			boolean isVariable = true;
+			sb.append("#").append(t).append(" ");
+			int n = Integer.parseInt(br.readLine());
+			char[] tree = new char[n + 1];
+			for(int i = 1; i <= n; i++) {
+				String[] a = br.readLine().split(" ");
+				tree[i] = a[1].charAt(0);
+			}
+			for(int i = 1; i <= n; i++) {
+				if(i * 2 + 1 <= n && Character.isDigit(tree[i])) {
+					isVariable = false;
+					break;
+				}
+				if(i * 2 + 1 > n && !Character.isDigit(tree[i])) {
+					isVariable = false;
+					break;
+				}
+			}
+			if(isVariable) sb.append(1).append("\n");
+			else sb.append(0).append("\n");	
+		}
+	System.out.println(sb);
+	}
+
+}
