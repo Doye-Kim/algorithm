b9b4c661addb9fc225bde68f9cc34390d05f0a54
김도예 <kmdy125@gmail.com>
2024-02-09 22:23:10 +0900

    [Silver IV] Title: 제로, Time: 244 ms, Memory: 24032 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int k = Integer.parseInt(br.readLine());
+		Deque<Integer> stack = new ArrayDeque<>();
+		for(int i =  0; i < k; i++) {
+			int n = Integer.parseInt(br.readLine());
+			if(n == 0) {
+				stack.pop();
+			}
+			else stack.push(n);
+		}
+		int ans = 0;
+		while(!stack.isEmpty()) {
+			ans += stack.pop();
+		}
+		System.out.println(ans);
+	}
+}
