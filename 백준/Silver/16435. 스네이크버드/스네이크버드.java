d5a045e55c226ef79e6b18178d30136b252bcb1c
김도예 <kmdy125@gmail.com>
2024-02-13 17:05:44 +0900

    [Silver V] Title: 스네이크버드, Time: 136 ms, Memory: 14500 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int l = Integer.parseInt(st.nextToken());
+		int[] fruits = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			fruits[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(fruits);
+		for (int i = 0; i < n; i++) {
+			if(fruits[i] <= l) l++;
+			else break;
+		}
+		System.out.println(l);
+	}
+
+}
