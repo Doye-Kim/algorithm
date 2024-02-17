43e8e2ab974584ad81cd69f7e8f824fb8bc547dc
김도예 <kmdy125@gmail.com>
2024-02-17 17:00:42 +0900

    [Gold IV] Title: 알파벳, Time: 808 ms, Memory: 15264 KB -BaekjoonHub

--- 백준/Gold IV/1987. 알파벳/알파벳.java
+++ 백준/Gold IV/1987. 알파벳/알파벳.java
@@ -1,40 +1,43 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
-	static char[][] board;
+	static int[][] board;
 	static int r, c;
 	static int max;
-	static Set<Character> set = new HashSet<>();
+	static boolean[] visited;
+
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		r = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
-		board = new char[r][c];
+		board = new int[r][c];
 		for(int i = 0; i < r; i++) {
-			board[i] = br.readLine().toCharArray();
+			String s = br.readLine();
+			for(int j = 0; j < c; j++) {
+				board[i][j] = s.charAt(j) - 'A';
+			}
 		}
+		visited = new boolean[26];
+		visited[board[0][0]] = true;
 		dfs(0, 0, 1);
-		//System.out.println(set);
+
 		System.out.println(max);
 	}
 	static void dfs(int x, int y, int cnt) {
-		//System.out.println(x + " " + y);
 		if(cnt > max) max = cnt;
-		set.add(board[x][y]);
 		for(int i = 0; i < 4; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
-			if(nx < 0 || ny < 0 || nx >= r || ny >= c || set.contains(board[nx][ny])) continue;
+			if(nx < 0 || ny < 0 || nx >= r || ny >= c || visited[board[nx][ny]]) continue;
+			visited[board[nx][ny]] = true;
 			dfs(nx, ny, cnt + 1);
-			set.remove(board[nx][ny]);
+			visited[board[nx][ny]] = false;
 		}
 		
 	}
