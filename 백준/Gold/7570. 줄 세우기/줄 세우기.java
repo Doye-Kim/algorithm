fdd25ac1d661a4ced640f6b9f5a854e74ceaeef0
김도예 <kmdy125@gmail.com>
2024-02-18 02:00:03 +0900

    [Gold III] Title: 줄 세우기, Time: 484 ms, Memory: 91220 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.java
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.java
@@ -1,21 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int n = Integer.parseInt(br.readLine());
 
-		int[] dp = new int[n+1];
-		for(int i = 0; i < n; i++) {
-			int k = Integer.parseInt(st.nextToken());
-			dp[k] = dp[k-1]+1;
-		}
-		Arrays.sort(dp);
-		System.out.println(n-dp[n]);
-	}
+        int[] arr = new int[n+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[Integer.parseInt(st.nextToken())] = i;
+        }
+
+        int max = 1;
+        int cnt = 0;
+        for (int i = 2; i <= n; i++) {
+            if (arr[i] > arr[i-1]) {
+                if (++cnt > max)
+                    max = cnt;
+            } else {
+                cnt = 1;
+            }
+        }
+        System.out.println(n-max);
+    }
 }
