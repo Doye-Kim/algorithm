0be94340f412b036284a14972fb372cbed8fd9cf
김도예 <kmdy125@gmail.com>
2024-02-25 00:39:48 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 732 ms, Memory: 70864 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -6,24 +6,23 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, E;
+
+	static int n, e, v1, v2;
 	static ArrayList<Node>[] list;
-	static int[][] weight;
-	public static void main(String[] args) throws Exception {
+	static int[][] weights;
+	static boolean[] visit;
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		list = new ArrayList[N + 1];
-		for(int i = 0; i <= N; i++){
-            list[i] = new ArrayList<>();
-        }
-		weight = new int[N + 1][N + 1];
-		for(int i = 0; i < N + 1; i++) {
-			Arrays.fill(weight[i], Integer.MAX_VALUE);			
+		n = Integer.parseInt(st.nextToken());
+		e = Integer.parseInt(st.nextToken());
+		list = new ArrayList[n + 1];
+		weights = new int[n + 1][n + 1];
+		for(int i = 0; i <= n; i++) {
+			list[i] = new ArrayList<>();
 		}
 		
-		for(int i = 0; i < E; i++) {
+		for(int i = 0; i < e; i++) {
 			st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
@@ -32,82 +31,73 @@ public class Main {
 			list[end].add(new Node(start, weight));
 		}
 		st = new StringTokenizer(br.readLine());
-		int v1 = Integer.parseInt(st.nextToken());
-		int v2 = Integer.parseInt(st.nextToken());
+		v1 = Integer.parseInt(st.nextToken());
+		v2 = Integer.parseInt(st.nextToken());
 		
-		// 1에서 출발, v1에서 출발, v2에서 출발
-//		dijk(1, v1) + dijk(v1, v2) + dijk(v2, N)
-//		dijk(1, v2) + dijk(v2, v1) + dijk(v1, N)
-		int[] visit = {1, v1, v2};
-		int[] ans1 = new int[3];
-		int[] ans2 = new int[3];
-		int cnt1 = 0;
-		int cnt2 = 0;
-		for(int i = 0; i < 3; i++) {
-			dijk(visit[i]);
+		for(int i = 0; i < n + 1; i++) {
+			if(i == 1 || i == v1 || i == v2) Arrays.fill(weights[i], Integer.MAX_VALUE);			
 		}
-		//System.out.println(Arrays.deepToString(weight));
-		ans1[0] = weight[1][v1];
-		ans2[0] = weight[1][v2];
-		ans1[1] = weight[v1][v2];
-		ans2[1] = weight[v1][v2];
-		ans1[2] = weight[v2][N];
-		ans2[2] = weight[v1][N];
+		// 1 -> v1 -> v2 -> n
+		// 1 -> v2 -> v1 -> n
+		// 시작점: 1, v1, v2
+		dijk(1);
+		dijk(v1);
+		dijk(v2);
 		
-		for (int i = 0; i < 3; i++) {
-			if(ans1[i] == Integer.MAX_VALUE) cnt1 = Integer.MAX_VALUE;
-			if(ans2[i] == Integer.MAX_VALUE) cnt2 = Integer.MAX_VALUE;
-		}
+		int case1 = 0;
+		int case2 = 0;
 		
-		int answer = 0;
+		int[] answer = new int[6];
 
-		if(cnt1 == Integer.MAX_VALUE && cnt2 == Integer.MAX_VALUE) {
-			answer = -1;
+		answer[0] = weights[1][v1];
+		answer[1] = weights[1][v2];
+		answer[2] = weights[v1][v2];
+		answer[3] = weights[v1][v2];
+		answer[4] = weights[v2][n];
+		answer[5] = weights[v1][n];
+		
+		for(int i = 0; i < 6; i++) {
+			if(i % 2 == 0) case1 += answer[i];
+			else case2 += answer[i];
 		}
-		if(cnt1 == 0){
-			for(int i = 0; i < 3; i++) {
-				cnt1 += ans1[i];
+		for(int i = 0; i < 6; i++) {
+			if(answer[i] == Integer.MAX_VALUE) {
+				if(i % 2 == 0) case1 = Integer.MAX_VALUE;
+				else case2 = Integer.MAX_VALUE;
 			}
 		}
-		if(cnt2 == 0) {
-			for(int i = 0; i < 3; i++) {
-				cnt2 += ans2[i];
-			}
-		}
-		if(answer == -1) System.out.println(answer);
-		else System.out.println(cnt1 > cnt2 ? cnt2 : cnt1);
+		int ans = Math.min(case1, case2);
+		if(case1 == Integer.MAX_VALUE && case2 == Integer.MAX_VALUE) ans = -1;
+
+		System.out.println(ans);
 	}
 	static void dijk(int start) {
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		pq.add(new Node(start, 0));
-		weight[start][start] = 0;
+		weights[start][start] = 0;
 		while(!pq.isEmpty()) {
-			Node pos = pq.poll();
-			int nx = pos.end;
-			int nw = pos.weight;
-
-			for(Node node : list[nx]) {
-				if(weight[start][node.end] > weight[start][nx] + node.weight) {
-					//System.out.println(weight[start][node.end] + " " + (weight[start][nx] + node.weight));
-					weight[start][node.end] = weight[start][nx] + node.weight;
-					pq.add(new Node(node.end, weight[start][node.end]));
-					//System.out.println(start + " " + node.end + " " + weight[start][node.end]);
+			Node node = pq.poll();
+			int nx = node.end;
+			for(Node n : list[node.end]) {
+				if(weights[start][n.end] > weights[start][nx] + n.weight) {
+					weights[start][n.end] = weights[start][nx] + n.weight;
+					pq.add(new Node(n.end, weights[start][n.end]));
 				}
 			}
 		}
 	}
-}
-class Node implements Comparable<Node>{
-	int end, weight;
+	static class Node implements Comparable<Node>{
+		int end, weight;
 
-	public Node(int end, int weight) {
-		this.end = end;
-		this.weight = weight;
-	}
+		public Node(int end, int weight) {
+			this.end = end;
+			this.weight = weight;
+		}
 
-	@Override
-	public int compareTo(Node o) {
-		return weight - o.weight;
+		@Override
+		public int compareTo(Node o) {
+			return weight - o.weight;
+		}
+		
 	}
-	
 }
