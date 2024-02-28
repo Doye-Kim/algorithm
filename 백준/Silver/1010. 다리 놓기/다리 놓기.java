6ed2ecc2670e93eb05359ddfbfd55e9170a9f6d1
김도예 <kmdy125@gmail.com>
2024-02-28 11:33:45 +0900

    [Silver V] Title: 다리 놓기, Time: 156 ms, Memory: 17828 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -3,27 +3,25 @@ import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.StringTokenizer;
 
-// boj 1010
 public class Main {
 
+	static int T, n, r;
+	static BigInteger[] dp = new BigInteger[31];
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
+		T = Integer.parseInt(br.readLine());
+		dp[0] = BigInteger.valueOf(1);
 		for(int t = 0; t < T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
-			
-			BigInteger answer = new BigInteger("1");
-			for(int i = m; i > m-n; i--) {
-				answer = answer.multiply(BigInteger.valueOf(i));
+			r = Integer.parseInt(st.nextToken());
+			n = Integer.parseInt(st.nextToken());
+			for(int i = 1; i <= n; i++) {
+				dp[i] = dp[i-1].multiply(BigInteger.valueOf(i));
 			}
-			for(int i = n; i > 0; i--) {
-				answer = answer.divide(BigInteger.valueOf(i));
-			}
-			sb.append(answer).append("\n");
+			sb.append((dp[n].divide(dp[r].multiply(dp[n-r])))).append("\n");
 		}
 		System.out.println(sb);
 	}
+
 }
