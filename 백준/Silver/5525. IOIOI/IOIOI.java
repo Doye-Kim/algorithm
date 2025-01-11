18ff9c018dd9f7736f8f3295da222cde344f31b3
김도예 <kmdy125@gmail.com>
2025-01-11 19:45:34 +0900

    [Silver I] Title: IOIOI, Time: 232 ms, Memory: 22084 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        char[] s = br.readLine().toCharArray();
+
+        int count = 0;
+        int result = 0;
+
+        for (int i = 1; i < m - 1; i++) {
+            if (s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
+                count++;
+                if (count >= n) {
+                    result++;
+                }
+                i++;
+            } else {
+                count = 0;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
