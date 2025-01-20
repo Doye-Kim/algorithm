565c3a9bc861beaeed9fb9ef4a45a881e9172be7
김도예 <kmdy125@gmail.com>
2025-01-20 12:15:00 +0900

    [Silver II] Title: 나무 자르기, Time: 476 ms, Memory: 119124 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,39 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    static int n, m, ans = 0;
+    static int[] trees;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int[] tree = new int[N];
-        st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        trees = new int [n];
+        st = new StringTokenizer(br.readLine());
         int max = 0;
-        for(int i = 0; i < N; i ++) {
-            tree[i] = Integer.parseInt(st.nextToken());
-            max = Math.max(max, tree[i]);
+        for(int i = 0; i < n; i++){
+            trees[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, trees[i]);
         }
-
-        int left = 0;
-        int right = max;
-
-        while(left < right) {
-            int mid = (right + left) / 2;
-            long length = 0;
-
-            for(int tree_len : tree) {
-                if(tree_len - mid > 0) length += tree_len - mid;
+        
+        binarySearch(0, max);
+        System.out.println(ans);
+    }
+    static void binarySearch(int start, int end){
+        while (start < end) {
+            int mid = (start + end) / 2;
+            
+            if (isCutEnough(mid)) {
+                start = mid + 1;
+            } else {
+                end = mid;
             }
-
-            if(length >= M) left = mid + 1;
-            else right = mid;
         }
-
-        System.out.println(left - 1);
+        ans = start - 1;
+    }
+    
+    static boolean isCutEnough(int height){
+        long sum = 0;
+        for(int i = 0; i < n; i++){
+            if(trees[i] > height) sum += trees[i] - height;
+        }
+        if(sum >= m) return true;
+        else return false;
     }
 }
