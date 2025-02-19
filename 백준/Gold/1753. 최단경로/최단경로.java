8902b43f58c53eef8555078d0bd53c15861d6f50
김도예 <kmdy125@gmail.com>
2025-02-20 00:46:46 +0900

    [Gold IV] Title: 최단경로, Time: 696 ms, Memory: 109252 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,62 +1,58 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-	static int v, e, start;
-	static int[] cost;
-	static StringBuilder sb = new StringBuilder();
-	static ArrayList<ArrayList<Node>> list = new ArrayList<>();
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		v = Integer.parseInt(st.nextToken());
-		e = Integer.parseInt(st.nextToken());	
-		cost = new int[v+1];
-		Arrays.fill(cost, Integer.MAX_VALUE);
-		start = Integer.parseInt(br.readLine());
-		for(int i = 0; i <= v; i++) {
-			list.add(new ArrayList<>());
-		}
-		for(int i = 0; i < e; i++) {
+    static int v, e;
+    static int[] dist;
+    static int max = Integer.MAX_VALUE;
+    static ArrayList<ArrayList<Node>> list = new ArrayList<>();;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        dist = new int[v+1];
+        Arrays.fill(dist, max);
+        for(int i = 0; i <= v; i++){
+            list.add(new ArrayList<>());
+        }
+        int start = Integer.parseInt(br.readLine());
+        for(int i = 0; i < e; i++) {
 			st = new StringTokenizer(br.readLine());
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 			list.get(u).add(new Node(v, w));
 		}
-		dijk();
-		for(int i = 1; i <= v; i++) {
-			sb.append(cost[i] == Integer.MAX_VALUE ? "INF" : cost[i]).append("\n");
+		dijk(start);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= v; i++){
+		    sb.append(dist[i] == max ? "INF" : dist[i]).append("\n");
 		}
 		System.out.println(sb);
-	}
-	static void dijk() {
-		PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2)-> n1.w - n2.w);
-		pq.offer(new Node(start, 0));
-		cost[start] = 0;
-		while(!pq.isEmpty()) {
-			Node pos = pq.poll();
-			int x = pos.v;
-			if(cost[x] < pos.w) continue;
-			for(Node node : list.get(x)) {
-				if(cost[node.v] > cost[x] + node.w) {
-					cost[node.v] = cost[x] + node.w;
-					pq.offer(new Node(node.v, cost[node.v]));
-				}
-			}
-		}
-	}
-	static class Node{
-		int v, w;
-
-		public Node(int v, int w) {
-			this.v = v;
-			this.w = w;
-		}
-	}
+    }
+    static void dijk(int start){
+        PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2)->n1.cost - n2.cost);
+        dist[start] = 0;
+        pq.offer(new Node(start, 0));
+        while(!pq.isEmpty()){
+            Node node = pq.poll();
+            if(dist[node.end] < node.cost) continue;
+            for(Node n: list.get(node.end)){
+                int nd = dist[node.end] + n.cost;
+                if(dist[n.end] > nd){
+                    pq.offer(new Node(n.end, nd));
+                    dist[n.end] = nd;
+                }
+            }
+        }
+        
+    }
+    static class Node{
+        int end, cost;
+        public Node(int end, int cost){
+            this.end = end;
+            this.cost = cost;
+        }
+    }
 }
