c5666b2e0ecf215a66179e426812013692ff3ed9
김도예 <kmdy125@gmail.com>
2025-03-12 13:19:33 +0900

    [Gold V] Title: 빗물, Time: 116 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -1,41 +1,38 @@
 import java.util.*;
 import java.io.*;
 
-class Main{
+public class Main {
+ 
+    static int h, w;
+    static int[] height;
+ 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int h = Integer.parseInt(st.nextToken());
         int w = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        
-        // 방법 1   
-        // w*h 2차원 배열을 만들어 가로 한 줄씩 순회하며
-        // 사이에 낀 개수를 찾음
-        // 시간 초과는 안 날 듯
-        // 왼쪽부터 블록이 있으면 그 이후 빈칸을 카운트 한다. 
-        // 막히면 그때까지의 빈 칸의 개수를 정답에 합함
-
-        boolean[][] blocks = new boolean[h][w];
-        for(int i = 0; i < w; i++){
-            int nh = Integer.parseInt(st.nextToken());
-            for(int j = h - 1; j >= h - nh; j--){
-                blocks[j][i] = true;
-            }
+ 
+        height = new int[w];
+        for(int i = 0; i < w; i++) {
+            height[i] = Integer.parseInt(st.nextToken());
         }
-        int ans = 0;
-        for(int i = 0; i < h; i++){
-            boolean flag = false;
-            int cnt = 0;
-            for(int j = 0; j < w; j++){
-                if(!flag && blocks[i][j]) flag = true;
-                else if(flag && !blocks[i][j]) cnt++;
-                else if(flag && blocks[i][j]) {
-                    ans += cnt;
-                    cnt = 0;
-                }
+ 
+        int result = 0;
+        for(int i = 1; i < w - 1; i++) {
+            int left = 0;
+            int right = 0;
+ 
+            for(int j = 0; j < i; j++) {
+                left = Math.max(height[j], left);
             }
+ 
+            for(int j = i + 1; j < w; j++) {
+                right = Math.max(height[j], right);
+            }
+ 
+            if(height[i] < left && height[i] < right) result += Math.min(left, right) - height[i];
         }
-        System.out.println(ans);
+        System.out.println(result);
     }
 }
