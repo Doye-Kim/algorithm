5ad6ac089521fb762af07eed6d029fc30967788a
김도예 <kmdy125@gmail.com>
2025-04-10 14:19:03 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 100 ms, Memory: 13992 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -2,14 +2,33 @@ import java.io.*;
 import java.util.*;
 public class Main
 {
-    static char[] vowels = {'a', 'e', 'i','o', 'u'};
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		while(true){
 		    String str = br.readLine();
 		    if(str.equals("end")) break;
-		    if(!containVowels(str) || isContinuous(str) || isRepeat(str)) {
+		    boolean flag = false;
+		    char prev = '-';
+		    int cnt = 0;
+		    for(int i = 0; i < str.length(); i++){
+		        char now = str.charAt(i);
+		        if(isVowel(now)) flag = true;
+		        
+		        if(isVowel(prev) != isVowel(now)) cnt = 1;
+		        else cnt++;
+		        
+		        if(cnt == 2 && prev == now && now != 'e' && now != 'o') {
+		            flag = false;
+		            break;
+		        }
+		        if(cnt >= 3) {
+		            flag = false;
+		            break;
+		        }
+		        prev = now;
+		    }
+		    if(!flag) {
 		        sb.append("<").append(str).append("> is not acceptable.\n");
 		    }
 		    else sb.append("<").append(str).append("> is acceptable.\n");
@@ -18,40 +37,8 @@ public class Main
 		
 		System.out.println(sb);
 	}
-	static boolean containVowels(String str){
-	    for(int i = 0; i < str.length(); i++){
-            for(int v = 0; v < 5; v++){
-                if(str.charAt(i) == vowels[v]) return true;
-            }
-        }
-        return false;
-	}
-	
-	static boolean isContinuous(String str){
-	    boolean prev = false;
-	    int cnt = 0;
-	    for(int i = 0; i < str.length(); i++){
-	        boolean now = false;
-            for(int v = 0; v < 5; v++){
-                if(str.charAt(i) == vowels[v]) now = true;
-            }
-            if(prev != now){
-                cnt = 1;
-            }
-            else cnt++;
-            if(cnt >= 3) return true;
-            prev = now;
-        }
-        return false;
-	}
-	static boolean isRepeat(String str){
-	    int cnt = 1;
-	    for(int i = 1; i < str.length(); i++){
-            if(str.charAt(i - 1) == str.charAt(i)) cnt++;
-            else cnt = 1;
-            
-            if(cnt >= 2 && str.charAt(i) != 'e' && str.charAt(i) != 'o') return true;
-        }
-        return false;
+	static boolean isVowel(char a){
+	    if(a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u') return true;
+	    return false;
 	}
 }
